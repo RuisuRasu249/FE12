d2ba7eb637dd229a3ca7c85d1e1b82a68ce7d2d3
Luis Ras <Ras-L@ulster.ac.uk>
2024-11-22 23:07:21 +0000

    Importing JSON data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import jsonData from '../assets/data.json';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'FE12';
+
+  ngOnInit(){
+    console.log(jsonData);
+  }
+
 }
